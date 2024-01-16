27b6899539f18590ce3985ffa957cd0bf0cc7c7f
Aleksandr Parshukov <leon040891@gmail.com>
2024-01-16 17:48:31 +0300

    Added getting the list of users

---
+++ Factory/UserFactory.cs
@@ -0,0 +1,96 @@
+﻿using BitrixApiNet.Api;
+using BitrixApiNet.Item;
+using BitrixApiNet.Item.Crm;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitrixApiNet.Factory
+{
+    public class UserFactory
+    {
+        private readonly ApiService _apiService;
+
+        public UserFactory(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public ICollection<UserItem> GetUsers()
+        {
+            return List<UserItem>(new QueryParameters());
+        }
+
+        public ICollection<UserItem> GetUsers(QueryParameters? queryParameters)
+        {
+            return List<UserItem>(queryParameters);
+        }
+
+        public ICollection<T> GetUsers<T>() where T : ItemBase
+        {
+            return List<T>(new QueryParameters());
+        }
+
+        public ICollection<T> GetUsers<T>(QueryParameters? queryParameters) where T : ItemBase
+        {
+            return List<T>(queryParameters);
+        }
+
+        private protected List<P> List<P>(QueryParameters? queryParameters) where P : ItemBase
+        {
+            return PrepareAndSendData<P>(queryParameters, "get");
+        }
+
+        private protected List<P> PrepareAndSendData<P>(QueryParameters? queryParameters, string method) where P:ItemBase
+        {
+            var list = new List<P>();
+            int lastId = 0;
+            var filter = queryParameters?.Filter ?? new();
+            filter.Add(">ID", lastId.ToString());
+            
+            while (true)
+            {
+                //TODO:Realize work with bitrix constraints
+                Thread.Sleep(1000);
+                
+                var body = new
+                {
+                    queryParameters?.Filter,
+                    start = -1,
+                    order = new { ID = "ASC" },
+                };
+                string responseString = SendPostAsync(method, body).Result;
+                var response = JsonConvert.DeserializeObject<ResultData<List<P>>>(responseString);
+
+                if (response is null || response.Result is null)
+                {
+                    break;
+                }
+                if(response.Result.LastOrDefault() is null)
+                {
+                    break;
+                }
+                if(lastId > (int)response.Result.LastOrDefault().Id)
+                    break;
+
+                lastId = (int)response.Result.LastOrDefault().Id;
+                filter[">ID"] = lastId.ToString();
+
+                list.AddRange(response.Result);
+            }
+            
+            
+            return list;
+        }
+
+        private protected async Task<string> SendPostAsync(string method, object body)
+        {
+            var jsonResult = _apiService.PostAsync($"user.{method}", body);
+
+            return await jsonResult;
+        }
+    }
+}
---
+++ Item/UserItem.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitrixApiNet.Item
+{
+    public class UserItem : ItemBase
+    {
+        [JsonProperty("ACTIVE")]
+        public bool? Active { get; set; }
+
+        [JsonProperty("EMAIL")]
+        public string? Email { get; set; }
+
+        [JsonProperty("NAME")]
+        public string? Name { get; set; }
+
+        [JsonProperty("LAST_NAME")]
+        public string? LastName { get; set; }
+
+        [JsonProperty("SECOND_NAME")]
+        public string? SecondName { get; set; }
+
+        [JsonProperty("PERSONAL_GENDER")]
+        public string? Gender { get; set; }
+
+        [JsonProperty("PERSONAL_PROFESSION")]
+        public string? Profession { get; set; }
+
+        [JsonProperty("PERSONAL_WWW")]
+        public string? WWW { get; set; }
+
+        [JsonProperty("BIRTHDAY")]
+        public DateTime? Birthday { get; set; }
+
+        [JsonProperty("PERSONAL_PHOTO")]
+        public string? Photo { get; set; }
+
+        [JsonProperty("PERSONAL_ICQ")]
+        public string? ICQ { get; set; }
+
+        [JsonProperty("PERSONAL_PHONE")]
+        public string? Phone { get; set; }
+
+        [JsonProperty("PERSONAL_FAX")]
+        public string? Fax { get; set; }
+
+        [JsonProperty("PERSONAL_MOBILE")]
+        public string? Mobile { get; set; }
+
+        [JsonProperty("PERSONAL_PAGER")]
+        public string? Pager { get; set; }
+
+        [JsonProperty("PERSONAL_STREET")]
+        public string? Street { get; set; }
+
+        [JsonProperty("PERSONAL_CITY")]
+        public string? City { get; set; }
+
+        [JsonProperty("PERSONAL_STATE")]
+        public string? State { get; set; }
+
+        [JsonProperty("PERSONAL_ZIP")]
+        public string? Zip { get; set; }
+
+        [JsonProperty("PERSONAL_COUNTRY")]
+        public string? Country { get; set; }
+
+        [JsonProperty("WORK_COMPANY")]
+        public string? WorkCompany { get; set; }
+
+        [JsonProperty("WORK_POSITION")]
+        public string? WorkPosition { get; set; }
+
+        //TODO: put it in a separate Department class
+        [JsonProperty("UF_DEPARTMENT")]
+        public List<int>? Department { get; set; }
+
+        [JsonProperty("UF_INTERESTS")]
+        public string? Interests { get; set; }
+
+        [JsonProperty("UF_SKILLS")]
+        public string? Skills { get; set; }
+
+        [JsonProperty("UF_WEB_SITES")]
+        public string? WebSites { get; set; }
+
+        [JsonProperty("UF_XING")]
+        public string? Xing { get; set; }
+
+        [JsonProperty("UF_LINKEDIN")]
+        public string? Linkedin { get; set; }
+
+        [JsonProperty("UF_FACEBOOK")]
+        public string? Facebook { get; set; }
+
+        [JsonProperty("UF_TWITTER")]
+        public string? Twitter { get; set; }
+
+        [JsonProperty("UF_SKYPE")]
+        public string? Skype { get; set; }
+
+        [JsonProperty("UF_DISTRICT")]
+        public string? District { get; set; }
+
+        [JsonProperty("UF_PHONE_INNER")]
+        public string? PhoneInner { get; set; }
+    }
+}
